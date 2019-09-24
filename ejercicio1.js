931108cd2bce6d937ec3e138056e6c587250f768
inigopees <iperezes18dw@ikzubirimanteo.com>
2019-09-24 11:33:55 +0200

    primer commit

---
+++ ejercicio1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<script>
+	var nombre = "asdf"
+var apellido = "poiu"
+var edad = 123
+var salario = 84359
+
+alert(nombre)
+</script>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
